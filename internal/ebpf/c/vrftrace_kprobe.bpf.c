bfdab2187f757d063aabbccf4804ff14c62a1777
shun159 <dreamdiagnosis@gmail.com>
2022-08-17 11:10:43 +0900

    formatted

--- internal/ebpf/c/vrftrace_kprobe.bpf.c
+++ internal/ebpf/c/vrftrace_kprobe.bpf.c
@@ -8,24 +8,23 @@
 static __inline uint64_t
 get_func_ip(void *ctx)
 {
-	return PT_REGS_IP((struct pt_regs *)ctx) - 1;
+  return PT_REGS_IP((struct pt_regs *)ctx) - 1;
 }
 
 #define KPROBE_NUM(ST_NAME, HANDLER_FUNC, NUM)                                 \
-	SEC("kprobe/##ST_NAME##NUM")                                           \
-	int ST_NAME##NUM(struct pt_regs *ctx)                                  \
-	{                                                                      \
-		struct ST_NAME *req =                                          \
-			(struct ST_NAME *)PT_REGS_PARM##NUM(ctx);              \
-		return HANDLER_FUNC(ctx, 0, req);                              \
-	}
+  SEC("kprobe/##ST_NAME##NUM")                                                 \
+  int ST_NAME##NUM(struct pt_regs *ctx)                                        \
+  {                                                                            \
+    struct ST_NAME *req = (struct ST_NAME *)PT_REGS_PARM##NUM(ctx);            \
+    return HANDLER_FUNC(ctx, 0, req);                                          \
+  }
 
 #define KPROBE(ST_NAME, HANDLER_FUNC)                                          \
-	KPROBE_NUM(ST_NAME, HANDLER_FUNC, 1)                                   \
-	KPROBE_NUM(ST_NAME, HANDLER_FUNC, 2)                                   \
-	KPROBE_NUM(ST_NAME, HANDLER_FUNC, 3)                                   \
-	KPROBE_NUM(ST_NAME, HANDLER_FUNC, 4)                                   \
-	KPROBE_NUM(ST_NAME, HANDLER_FUNC, 5)
+  KPROBE_NUM(ST_NAME, HANDLER_FUNC, 1)                                         \
+  KPROBE_NUM(ST_NAME, HANDLER_FUNC, 2)                                         \
+  KPROBE_NUM(ST_NAME, HANDLER_FUNC, 3)                                         \
+  KPROBE_NUM(ST_NAME, HANDLER_FUNC, 4)                                         \
+  KPROBE_NUM(ST_NAME, HANDLER_FUNC, 5)
 
 KPROBE(sk_buff, handle_sk_buff)
 KPROBE(vr_packet, handle_vr_packet)
